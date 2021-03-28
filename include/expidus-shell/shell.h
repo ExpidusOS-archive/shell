fed6639b892c576540c0c688c064fdacceddb402
Tristan Ross <rosscomputerguy@protonmail.com>
2021-03-28 08:49:58 -0700

    Fixed missing headers

---
+++ include/expidus-shell/shell.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define EXPIDUS_TYPE_SHELL expidus_shell_get_type()
+G_DECLARE_FINAL_TYPE(ExpidusShell, expidus_shell, EXPIDUS, SHELL, GObject);
+
+struct _ExpidusShell {
+	GObject parent_instance;
+};
+
+GList* expidus_shell_get_desktops(ExpidusShell* shell);
+void expidus_shell_sync_desktops(ExpidusShell* self);
+
+G_END_DECLS
