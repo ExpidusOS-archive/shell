9556c47c040228f8a393803875af2cc5bdd00478
Tristan Ross <rosscomputerguy@protonmail.com>
2021-03-27 22:13:10 -0700

    Panel now works

--- src/desktop.c
+++ src/desktop.c
@@ -3,14 +3,24 @@
 #include <flutter_linux/flutter_linux.h>
 #include <meta/display.h>
 #include <meta/meta-plugin.h>
+#define WNCK_I_KNOW_THIS_IS_UNSTABLE 1
+#include <libwnck/libwnck.h>
 #include <expidus-build.h>
 #include <flutter.h>
 
 typedef struct {
 	FlDartProject* proj;
   FlView* view;
+  FlMethodChannel* channel_dart;
+
 	int monitor_index;
 	ExpidusShell* shell;
+  GSList* struts;
+
+  WnckScreen* screen;
+  gulong sig_win_changed;
+  gulong sig_app_closed;
+  gulong sig_app_opened;
 } ExpidusShellDesktopPrivate;
 G_DEFINE_TYPE_WITH_PRIVATE(ExpidusShellDesktop, expidus_shell_desktop, GTK_TYPE_WINDOW);
 
@@ -22,6 +32,101 @@ enum {
 };
 
 static GParamSpec* obj_props[N_PROPS] = { NULL };
+
+static void expidus_shell_desktop_set_current_app_cb(GObject* obj, GAsyncResult* result, gpointer data) {
+  ExpidusShellDesktop* self = EXPIDUS_SHELL_DESKTOP(data);
+  ExpidusShellDesktopPrivate* priv = expidus_shell_desktop_get_instance_private(self);
+
+  GError* error = NULL;
+  FlMethodResponse* resp = fl_method_channel_invoke_method_finish(priv->channel_dart, result, &error);
+
+  if (resp == NULL) {
+    g_warning("Failed to call method: %s", error->message);
+    return;
+  }
+
+  if (fl_method_response_get_result(resp, &error) == NULL) {
+    g_warning("Method returned error: %s", error->message);
+    return;
+  }
+}
+
+static void expidus_shell_desktop_app_closed(WnckScreen* screen, WnckApplication* app, gpointer data) {
+  ExpidusShellDesktop* self = EXPIDUS_SHELL_DESKTOP(data);
+  ExpidusShellDesktopPrivate* priv = expidus_shell_desktop_get_instance_private(self);
+
+  FlValue* args = fl_value_new_list();
+  fl_value_append(args, fl_value_new_bool(FALSE));
+
+  fl_method_channel_invoke_method(priv->channel_dart, "setCurrentApplication", args, NULL, expidus_shell_desktop_set_current_app_cb, self);
+}
+
+static gchar* cache_appicon(WnckApplication* app) {
+  const gchar* name = wnck_application_get_name(app);
+  if (wnck_application_get_icon(app) != NULL) {
+    gchar* path = g_build_filename(g_get_tmp_dir(), g_strdup_printf("expidus-shell-appcache-%s-icon.png", name), NULL);
+    GError* error = NULL;
+    if (!gdk_pixbuf_save(wnck_application_get_icon(app), path, "png", &error, NULL)) {
+      g_warning("Failed to cache icon: %s", error->message);
+      return NULL;
+    } else {
+      return path;
+    }
+  }
+  if (wnck_application_get_mini_icon(app) != NULL) {
+    gchar* path = g_build_filename(g_get_tmp_dir(), g_strdup_printf("expidus-shell-appcache-%s-mini-icon.png", name), NULL);
+    GError* error = NULL;
+    if (!gdk_pixbuf_save(wnck_application_get_mini_icon(app), path, "png", &error, NULL)) {
+      g_warning("Failed to cache icon: %s", error->message);
+      return NULL;
+    } else {
+      return path;
+    }
+  }
+  return NULL;
+}
+
+static void expidus_shell_desktop_app_opened(WnckScreen* screen, WnckApplication* app, gpointer data) {
+  ExpidusShellDesktop* self = EXPIDUS_SHELL_DESKTOP(data);
+  ExpidusShellDesktopPrivate* priv = expidus_shell_desktop_get_instance_private(self);
+
+  FlValue* args = fl_value_new_list();
+  const gchar* name = wnck_application_get_name(app);
+  if (!g_strcmp0(name, "expidus-shell.bin")) {
+    fl_value_append(args, fl_value_new_bool(FALSE));
+  } else {
+    fl_value_append(args, fl_value_new_bool(TRUE));
+    fl_value_append(args, fl_value_new_string(name));
+    const gchar* icon_name = wnck_application_get_icon_name(app);
+    if (icon_name != NULL) {
+      GtkIconTheme* icon_theme = gtk_icon_theme_get_default();
+      GtkIconInfo* icon_info = gtk_icon_theme_lookup_icon(icon_theme, icon_name, 32, GTK_ICON_LOOKUP_NO_SVG);
+      if (icon_info != NULL) {
+        g_debug("Loaded icon info (non-svg image): %s", icon_name);
+        fl_value_append(args, fl_value_new_string(gtk_icon_info_get_filename(icon_info)));
+        g_object_unref(icon_info);
+      } else {
+        gchar* icon_path = cache_appicon(app);
+        if (icon_path != NULL) fl_value_append(args, fl_value_new_string(icon_path));
+      }
+    } else {
+      gchar* icon_path = cache_appicon(app);
+      if (icon_path != NULL) fl_value_append(args, fl_value_new_string(icon_path));
+    }
+  }
+
+  fl_method_channel_invoke_method(priv->channel_dart, "setCurrentApplication", args, NULL, expidus_shell_desktop_set_current_app_cb, self);
+}
+
+static void expidus_shell_desktop_win_changed(WnckScreen* screen, WnckWindow* prev_win, gpointer data) {
+  WnckWindow* win = wnck_screen_get_active_window(screen);
+  WnckApplication* app = win != NULL ? wnck_window_get_application(win) : NULL;
+  if (win == NULL) {
+    expidus_shell_desktop_app_closed(screen, NULL, data);
+  } else {
+    expidus_shell_desktop_app_opened(screen, app, data);
+  }
+}
 
 static void expidus_shell_desktop_set_property(GObject* obj, guint prop_id, const GValue* value, GParamSpec* pspec) {
   ExpidusShellDesktop* self = EXPIDUS_SHELL_DESKTOP(obj);
@@ -82,6 +187,7 @@ static void expidus_shell_desktop_constructed(GObject* obj) {
   gtk_window_set_default_size(win, rect.width, rect.height);
   gtk_window_set_resizable(win, FALSE);
   gtk_widget_set_size_request(GTK_WIDGET(win), rect.width, rect.height);
+  gtk_window_set_accept_focus(win, FALSE);
 
 	priv->proj = fl_dart_project_new();
 
@@ -100,6 +206,22 @@ static void expidus_shell_desktop_constructed(GObject* obj) {
 
   gtk_container_add(GTK_CONTAINER(win), GTK_WIDGET(priv->view));
 	gtk_widget_show_all(GTK_WIDGET(self));
+
+  MetaStrut* strut = g_slice_new0(MetaStrut);
+  g_assert(strut);
+  strut->side = META_SIDE_TOP;
+  strut->rect = meta_rect(rect.x, rect.y, rect.height, 30);
+  priv->struts = g_slist_append(priv->struts, strut);
+
+  FlEngine* engine = fl_view_get_engine(priv->view);
+  FlBinaryMessenger* binmsg = fl_engine_get_binary_messenger(engine);
+  priv->channel_dart = fl_method_channel_new(binmsg, "com.expidus.shell/desktop.dart", FL_METHOD_CODEC(fl_standard_method_codec_new()));
+
+  priv->screen = wnck_screen_get_default();
+  g_assert(priv->screen);
+  priv->sig_win_changed = g_signal_connect(priv->screen, "active-window-changed", G_CALLBACK(expidus_shell_desktop_win_changed), self);
+  priv->sig_app_closed = g_signal_connect(priv->screen, "application-closed", G_CALLBACK(expidus_shell_desktop_app_closed), self);
+  priv->sig_app_opened = g_signal_connect(priv->screen, "application-opened", G_CALLBACK(expidus_shell_desktop_app_opened), self);
 }
 
 static void expidus_shell_desktop_dispose(GObject* obj) {
@@ -108,6 +230,16 @@ static void expidus_shell_desktop_dispose(GObject* obj) {
 
   g_clear_object(&priv->view);
   g_clear_object(&priv->proj);
+
+  if (priv->sig_app_closed > 0) {
+    g_signal_handler_disconnect(priv->screen, priv->sig_app_closed);
+    priv->sig_app_closed = 0;
+  }
+
+  if (priv->sig_app_opened > 0) {
+    g_signal_handler_disconnect(priv->screen, priv->sig_app_opened);
+    priv->sig_app_opened = 0;
+  }
 
   G_OBJECT_CLASS(expidus_shell_desktop_parent_class)->dispose(obj);
 }
@@ -126,3 +258,10 @@ static void expidus_shell_desktop_class_init(ExpidusShellDesktopClass* klass) {
 }
 
 static void expidus_shell_desktop_init(ExpidusShellDesktop* self) {}
+
+GSList* expidus_shell_desktop_get_struts(ExpidusShellDesktop* self) {
+  g_return_val_if_fail(EXPIDUS_SHELL_IS_DESKTOP(self), NULL);
+  ExpidusShellDesktopPrivate* priv = expidus_shell_desktop_get_instance_private(self);
+  g_return_val_if_fail(priv, NULL);
+  return priv->struts;
+}
