20174359af6e4cbed60f23fd94ff5a541a72d543
TheComputerGuy <rosscomputerguy@protonmail.com>
2021-04-01 22:07:05 -0700

    Working on the dashboard's user drawer

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 import 'package:dbus/dbus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:posix/posix.dart';
+import 'package:shell/dbus/org.freedesktop.Accounts-remote.dart';
+import 'package:shell/dbus/org.freedesktop.Accounts.User-remote.dart';
 
 enum DashboardUIStartupMode { NONE, LEFT_DRAWER, RIGHT_DRAWER }
 
@@ -19,6 +22,10 @@ class DashboardUI extends StatefulWidget {
 class _DashboardUIState extends State<DashboardUI> {
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   MethodChannel _channel = MethodChannel('com.expidus.shell/dashboard');
+  DBusClient _dBusClient = DBusClient.system();
+  OrgFreedesktopAccounts _accounts = OrgFreedesktopAccounts(
+      DBusClient.system(), 'org.freedesktop.Accounts',
+      path: DBusObjectPath('/org/freedesktop/Accounts'));
 
   final String backgroundPath;
   final DashboardUIStartupMode startupMode;
@@ -27,7 +34,6 @@ class _DashboardUIState extends State<DashboardUI> {
   @override
   void initState() {
     super.initState();
-    // TODO: open drawers if the right state is set on startup
     if (startupMode != DashboardUIStartupMode.NONE) {
       Future.microtask(() => startupMode == DashboardUIStartupMode.LEFT_DRAWER
           ? _scaffoldKey.currentState.openDrawer()
@@ -35,12 +41,48 @@ class _DashboardUIState extends State<DashboardUI> {
     }
   }
 
+  Future<Widget> createUserWidget() async {
+    final user = await this._accounts.callFindUserById(getuid()).then(
+        (String value) => OrgFreedesktopAccountsUser(
+            this._accounts.client, 'org.freedesktop.Accounts',
+            path: DBusObjectPath(value)));
+
+    final icon = await user.getIconFile();
+    return DrawerHeader(
+        child: new ListView(children: [
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Padding(
+            padding: const EdgeInsets.only(left: 2.0, right: 2.0),
+            child: icon != null
+                ? CircleAvatar(foregroundImage: FileImage(new File(icon)))
+                : CircleAvatar(child: Icon(Icons.person))),
+        Padding(
+            padding: const EdgeInsets.only(left: 2.0, right: 2.0),
+            child: Text(await user.getRealName()))
+      ])
+    ]));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         key: _scaffoldKey,
         drawer: Drawer(),
-        endDrawer: Drawer(),
+        endDrawer: new Drawer(
+            child: new ListView(
+          children: [
+            FutureBuilder<Widget>(
+                future: this.createUserWidget(),
+                builder: (BuildContext context, AsyncSnapshot<Widget> data) {
+                  if (data.hasData) {
+                    return data.data;
+                  } else if (data.hasError) {
+                    throw data.error;
+                  }
+                  return CircularProgressIndicator();
+                })
+          ],
+        )),
         body: TextButton(
             onPressed: () => _channel.invokeMethod('hideDashboard'),
             child: Container(
